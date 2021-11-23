a3701bf9b66a5c4964b256fac047bd6e9ad7eb26
Mantecorso <luisjuradoquesada@gmail.com>
2021-11-23 17:42:25 +0100

    ready

--- menuani.js
+++ menuani.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', function() {
-    
+    let menumov;
+    menumov = this.document.getElementById('menuMov');
+    menumov.addEventListener('click', function() {
+
+        menumov.classList.toggle('active');
+
+    })
 })
