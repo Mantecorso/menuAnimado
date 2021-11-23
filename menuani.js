249db155d5c23271d8778e8c09248d59b9fca2a8
Mantecorso <luisjuradoquesada@gmail.com>
2021-11-23 20:26:04 +0100

    menu toggle

--- menuani.js
+++ menuani.js
@@ -6,4 +6,12 @@ window.addEventListener('load', function() {
         menumov.classList.toggle('active');
 
     })
+
+    let tog3, grande;
+    grande = this.document.getElementById('gran')
+    tog3 = this.document.getElementById('abce');
+    tog3.addEventListener('click', function() {
+        tog3.classList.toggle('active');
+        grande.classList.toggle('active');
+    })
 })
